60f5d40288bf743c2ea27e90717e27a64d30737a
Phellype <phellypeaugustho@gmail.com>
2022-11-23 19:31:02 -0300

    primeiro commit

---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,20 @@
+
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+    abilities = [];
+    height;
+    weight;
+    stats = [];
+    totalStats;
+    speciesUrl;
+    species;
+    growthRate;
+    habitat;
+    eggGroups = [];
+    moves = [];
+
+}
